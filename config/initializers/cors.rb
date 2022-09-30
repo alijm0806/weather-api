c2ac8a9b7f6dd01371261bdd5c8996eeb65f6f60
alijm0806 <alijm1104@gmail.com>
2022-09-30 12:28:22 -0500

    Add domain name to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "localhost:8080", "jocular-cocada-470d9b.netlify.app"
 
     resource "*",
       headers: :any,
