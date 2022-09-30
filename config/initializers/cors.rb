fd0080d425b260b70bdd7eaf3c2570630dc81c49
alijm0806 <alijm1104@gmail.com>
2022-09-30 12:42:07 -0500

    Fix cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "jocular-cocada-470d9b.netlify.app"
+    origins "localhost:8080", "https://jocular-cocada-470d9b.netlify.app"
 
     resource "*",
       headers: :any,
